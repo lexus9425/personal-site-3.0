221d0c78a7b975cbf104e43c1bd3ce84f8c23b2e
Alexis Ojeda <aojeda@JKLI.COM>
2022-09-13 16:00:05 -0400

    Remove header. Add text to About Me section.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './App.css';
 
-import Menu from './components/Menu';
-import Header from './components/Header';
 import Intro from './pages/Intro';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Footer from './components/Footer';
 
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
-      <div className='relative'>
-        <Menu isOpen={isOpen} setIsOpen={setIsOpen} />
-        <Header Name={"Alexis"} isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div>
       </div>
       <Intro Name={"Alexis"} />
-      <About Copy={"TBD"} />
+      <About />
       <Projects />
       <Footer email={"lexus9425@gmail.com"} />
     </div>
