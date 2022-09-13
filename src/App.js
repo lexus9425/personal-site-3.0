e3beb5c8b26c0768ec61c5d9328d0f6de2848dbe
Alexis Ojeda <aojeda@JKLI.COM>
2022-09-13 16:24:09 -0400

    ignore this commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
+import Menu from './components/Menu';
+import Header from './components/Header';
 import Intro from './pages/Intro';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Footer from './components/Footer';
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
-      <div>
+      <div className='relative'>
+        <Menu isOpen={isOpen} setIsOpen={setIsOpen} />
+        <Header Name={"Alexis"} isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
       <Intro Name={"Alexis"} />
-      <About />
+      <About Copy={"TBD"} />
       <Projects />
       <Footer email={"lexus9425@gmail.com"} />
     </div>
