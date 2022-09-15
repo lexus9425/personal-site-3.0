a9ab9a65829c1634ea8b7587631a92da1fad1416
Alexis Ojeda <aojeda@JKLI.COM>
2022-09-15 15:58:39 -0400

    Fixed animations in Intro.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './App.css';
+import styles from './styles/header.css';
 
-import Header from './components/Header'
 import Intro from './pages/Intro';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Footer from './components/Footer';
 
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
       <div className='relative'>
       </div>
       <Intro Name={"Alexis"} />
-      <About Copy={"TBD"} />
+      <About />
       <Projects />
       <Footer email={"lexus9425@gmail.com"} />
     </div>
