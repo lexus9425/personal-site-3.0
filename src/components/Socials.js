a39f7b75a1ddff01074f4cbfeb5bf5a730e5ea70
Alexis Ojeda <aojeda@JKLI.COM>
2022-09-16 13:33:46 -0400

    Increase size of social media icons.

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -6,19 +6,19 @@ const Socials = () => {
   return (
     <div className={styles.socials}>
       <a rel='noreferrer' href={"https://www.instagram.com/lexus9425/"} target='blank'>
-        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-instagram fa-lg"></i>
       </a>
 
       <a rel='noreferrer' href={"https://twitter.com/Lexus9425"} target='blank'>
-        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-twitter fa-lg"></i>
       </a>
       
       <a rel='noreferrer' href={"https://www.linkedin.com/in/alexis-ojeda-b912578b/"} target='blank'>
-        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-linkedin fa-lg"></i>
       </a>
       
       <a rel='noreferrer' href={"https://github.com/lexus9425"} target='blank'>
-        <i class="fa-brands fa-github"></i>
+        <i class="fa-brands fa-github fa-lg"></i>
       </a>
     </div>
   );
